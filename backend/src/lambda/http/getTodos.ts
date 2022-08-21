20abb55b920f32c0932e06f4356fd385a1ff4130
Debola <adebolasanni079@gmail.com>
2022-08-22 00:25:46 +0100

    get todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,24 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { getAllTodoForUser } from '../../businessLogic/Todo'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
-export const handler = middy(
+export const handler = 
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    console.log("Processing event", event)
+    const authorization = event.headers.Authorization
+    const jwtToken = authorization.split(" ")[1]
+    const todos = await getAllTodoForUser(jwtToken)
 
-    return undefined
-
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
