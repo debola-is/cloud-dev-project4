ef7e3af80d8bdd10118df364735dddaf505a0fa9
debola-is <adebolasanni079@gmail.com>
2022-09-01 11:51:52 +0100

    create todo and get todos function in serverless.yaml

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,33 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
-export const handler = middy(
+export const handler = (
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    console.log("Processing event: ", event)
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(" ")
+    const jwtToken = split[1]
 
-    return undefined
+    const todos = await getAllTodo(jwtToken);
 
-handler.use(
-  cors({
-    credentials: true
-  })
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Äccess-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        "items": todos
+      })
+    }
+  }
 )
+
+
