30e6a429605e3bc08eac68da47203db372bab46e
debola-is <adebolasanni079@gmail.com>
2022-09-02 17:20:49 +0100

    gatewayresponsedefault resource fix

--- backend/src/businessLogic/Todo.ts
+++ backend/src/businessLogic/Todo.ts
@@ -3,7 +3,7 @@ import {parseUserId} from "../auth/utils";
 import {CreateTodoRequest} from "../requests/CreateTodoRequest";
 import {UpdateTodoRequest} from "../requests/UpdateTodoRequest";
 import {TodoUpdate} from "../models/TodoUpdate";
-import {ToDoAccess} from "../dataLayer/ToDoAccess";
+import {ToDoAccess} from "../dataLayer/TodoAccess";
 
 const uuidv4 = require('uuid/v4');
 const toDoAccess = new ToDoAccess();
