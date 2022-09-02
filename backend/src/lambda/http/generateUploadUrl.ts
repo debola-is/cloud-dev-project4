7cb261b282750fc3fb2d341e75b3f0dee97f36ac
debola-is <adebolasanni079@gmail.com>
2022-09-02 15:33:07 +0100

    generate upload url complete

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,13 +3,13 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+import { generateUploadUrl } from '../../businessLogic/Todo'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
      const URL = await generateUploadUrl(todoId)
-
     return {
       statusCode: 202,
       headers: {
